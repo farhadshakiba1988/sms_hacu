3ee6c00e0fff7ce75ecc1428c310d1ab346d8c6c
farhad <farhadshakiba@yahoo.com>
2021-10-07 18:00:44 +0330

    add functions get_sms, send_sms, ckeck_serial

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ app = Flask(__name__)
 def main_page():
     ''' this is a main page of site '''
     return 'Hello'
+
+
+@app.route('/v1/get_sms')
+def get_sms():
+    pass
+
+
+@app.route('/v1/send_sms')
+def send_sms():
+    pass
+
+
+@app.route('/v1/check_serial')
+def check_serial():
+    pass
